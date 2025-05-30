325afb06e688d06dd49ec96a09e34fb8c9d815fd
Fanny HUJMAN <hujman.fanny@gmail.com>
2025-05-30 09:27:20 +0200

    Added logo Orkestre on the NavBar

--- orkestre-app/src/app/app.routes.ts
+++ orkestre-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CardEvenementComponent } from './pages/card-evenement/card-evenement.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { EvenementFormComponent } from './pages/evenement-form/evenement-form.component';
 
 export const routes: Routes = [
 
@@ -11,5 +12,9 @@ export const routes: Routes = [
     {
         path:'ShowAllEvenements',
         component:CardEvenementComponent
+    },
+     {
+        path:'CreateEvenement',
+        component:EvenementFormComponent
     },
 ];
